a8bffb650545b43417d4435bab188b125cddba91
Kirby T. Wu <tkirby@gmail.com>
2013-09-14 00:02:03 +0800

    add snapshot: qa explorer function panel

--- src/ls/index.ls
+++ src/ls/index.ls
@@ -1,3 +1,11 @@
 set-menu = (v,u) ->
   $ "\#navbar li:nth-child(#{v})" .addClass \active
   $ "\#menu > li:nth-child(#{u})" .addClass \active
+
+qa-func-resize = ->
+  $(".qa-func-panel")height -> $(this)prev!height!
+window.onresize = ->
+  qa-func-resize!
+
+<- $ document .ready 
+qa-func-resize!
--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,16 @@
-var setMenu;
+var setMenu, qaFuncResize;
 setMenu = function(v, u){
   $("#navbar li:nth-child(" + v + ")").addClass('active');
   return $("#menu > li:nth-child(" + u + ")").addClass('active');
 };
+qaFuncResize = function(){
+  return $(".qa-func-panel").height(function(){
+    return $(this).prev().height();
+  });
+};
+window.onresize = function(){
+  return qaFuncResize();
+};
+$(document).ready(function(){
+  return qaFuncResize();
+});
