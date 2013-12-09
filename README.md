cbfa7e1f6c5e453fa97062a186a78a575f785b76
ETBlue <etblue.tw@gmail.com>
2013-12-09 15:49:51 +0800

    organize templates, add tag list view etc

--- README.md
+++ README.md
@@ -10,3 +10,8 @@ https://docs.google.com/folder/d/0B0NsS2a-Qx8Za0x6WURKTzcxeFk/edit?docId=0B0NsS2
 License
 ============
 License: http://g0v.mit-license.org/ , although it is empty so far.
+
+
+compile jade to html: 
+
+* jade -w src -o .
--- js/main.js
+++ js/main.js
@@ -1,2 +1,30 @@
 //$('.ui.dropdown').dropdown({'on':'hover'});
 $('.ui.dropdown').dropdown();
+
+$('#header>.menu>.right>.dropdown>.menu>.relation').on('click tap',function(){
+  $('#content .category .relation, #header>.menu>.right>.dropdown>.relation').show();
+  $('#content .category .tag, #header>.menu>.right>.dropdown>.tag').hide();
+  $('#content .category .board, #header>.menu>.right>.dropdown>.board').hide();
+  $('#header>.menu>.right>.dropdown>.no.category').hide();
+})
+
+$('#header>.menu>.right>.dropdown>.menu>.tag').on('click tap',function(){
+  $('#content .category .relation, #header>.menu>.right>.dropdown>.relation').hide();
+  $('#content .category .tag, #header>.menu>.right>.dropdown>.tag').show();
+  $('#content .category .board, #header>.menu>.right>.dropdown>.board').hide();
+  $('#header>.menu>.right>.dropdown>.no.category').hide();
+})
+
+$('#header>.menu>.right>.dropdown>.menu>.board').on('click tap',function(){
+  $('#content .category .relation, #header>.menu>.right>.dropdown>.relation').hide();
+  $('#content .category .tag, #header>.menu>.right>.dropdown>.tag').hide();
+  $('#content .category .board, #header>.menu>.right>.dropdown>.board').show();
+  $('#header>.menu>.right>.dropdown>.no.category').hide();
+})
+
+$('#header>.menu>.right>.dropdown>.menu>.no.category').on('click tap',function(){
+  $('#content .category .relation, #header>.menu>.right>.dropdown>.relation').hide();
+  $('#content .category .board, #header>.menu>.right>.dropdown>.board').hide();
+  $('#content .category .tag, #header>.menu>.right>.dropdown>.tag').hide();
+  $('#header>.menu>.right>.dropdown>.no.category').show();
+})
