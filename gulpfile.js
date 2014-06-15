20c7c3c9486cf4475157b455d096e471a90d1664
Julian_Chu <walkingice@0xlab.org>
2014-06-16 00:06:44 +0800

    get rid of livereload
    well, I do not like live-reload. of course you are welcome to add
    it back as your wish.

--- gulpfile.js
+++ gulpfile.js
@@ -83,7 +83,6 @@ gulp.task('default', ['clean'], function () {
 gulp.task('connect', function () {
     var connect = require('connect');
     var app = connect()
-        .use(require('connect-livereload')({ port: 35729 }))
         .use(connect.static('app'))
         .use(connect.static(TMP))
         .use(connect.directory('app'));
@@ -118,19 +117,6 @@ gulp.task('wiredep', function () {
 });
 
 gulp.task('watch', ['connect', 'serve'], function () {
-    var server = $.livereload();
-
-    // watch for changes
-
-    gulp.watch([
-        'app/*.html',
-        TMP + '/styles/**/*.css',
-        'app/scripts/**/*.js',
-        'app/images/**/*'
-    ]).on('change', function (file) {
-        server.changed(file.path);
-    });
-
     gulp.watch('app/styles/**/*.scss', ['styles']);
     gulp.watch('app/scripts/**/*.js', ['scripts']);
     gulp.watch('app/images/**/*', ['images']);
