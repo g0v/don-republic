3a104e0a98623efdc526fbef2b4d799687e6aed5
Julian_Chu <walkingice@0xlab.org>
2014-06-21 15:42:25 +0800

    js: impement Login by Firebase simple login
    load firebase js library and use angular-firebase to implement
    firebase simple login.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,37 @@
 (function (angular) {
     var app;
 
-    app = angular.module('DonRepublic', []);
+    app = angular.module('DonRepublic', ['firebase']);
 
-    app.controller('LoginCtrl', function ($scope) {
-        $scope.loggedIn = false;
+    app.constant('Config', {
+        'firebaseApp': 'https://don-alpha.firebaseio.com'
     });
+
+    app.controller('LoginCtrl', ['$scope', '$rootScope', 'Config', '$firebaseSimpleLogin', function ($scope, $rootScope, Config, $firebaseSimpleLogin) {
+        var ref = new Firebase(Config.firebaseApp),
+            auth = $firebaseSimpleLogin(ref),
+            checkUser;
+
+        checkUser = function (user) {
+            $scope.loggedIn = !!user;
+        };
+
+        auth.$getCurrentUser().then(checkUser);
+
+        $scope.login = function () {
+            auth.$login('facebook');
+        };
+
+        $scope.logout = function () {
+            auth.$logout();
+        };
+
+        $rootScope.$on('$firebaseSimpleLogin:login', function (e, user) {
+            checkUser(user);
+        });
+
+        $rootScope.$on('$firebaseSimpleLogin:logout', function (e, user) {
+            checkUser(user);
+        });
+    }]);
 })(window.angular);
