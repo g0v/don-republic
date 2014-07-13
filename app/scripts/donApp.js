4875a0b463904a44b9ffe18546f8e24b45aad364
Julian_Chu <walkingice@0xlab.org>
2014-07-13 23:54:12 +0800

    ui.router: add more partials

--- app/scripts/donApp.js
+++ app/scripts/donApp.js
@@ -18,5 +18,29 @@
                 'mainView': {templateUrl: "default/main.html"}
             }
         });
+        $stateProvider.state('issues', {
+            url: '/issues',
+            views: {
+                'mainView': {templateUrl: "issues/main.html"}
+            }
+        });
+        $stateProvider.state('proposals', {
+            url: '/proposals',
+            views: {
+                'mainView': {templateUrl: "proposals/main.html"}
+            }
+        });
+        $stateProvider.state('projects', {
+            url: '/projects',
+            views: {
+                'mainView': {templateUrl: "projects/main.html"}
+            }
+        });
+        $stateProvider.state('tags', {
+            url: '/tags',
+            views: {
+                'mainView': {templateUrl: "tags/main.html"}
+            }
+        });
     }]);
 })(window.angular, window.Firebase, window.runtimeConfig);
